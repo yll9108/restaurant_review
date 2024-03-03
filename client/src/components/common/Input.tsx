55510c3755127b7ebfb4cf73edcb92ea981aeb1d
masashi381 <121969638+masashi381@users.noreply.github.com>
2024-03-03 09:56:20 -0800

    create common input component

---
+++ client/src/components/common/Input.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+export enum TextType {
+  text,
+  password,
+  email,
+}
+
+export type TextProps = {
+  textType: TextType;
+  className?: string;
+  placeholder?: string;
+};
+
+export default function Input(props: TextProps) {
+  let type = "";
+  switch (props.textType) {
+    case TextType.text:
+      type = "text";
+      break;
+    case TextType.password:
+      type = "password";
+      break;
+    case TextType.email:
+      type = "email";
+      break;
+    default:
+      type = "text";
+      break;
+  }
+  return (
+    <input
+      type={type}
+      placeholder={props.placeholder}
+      className="input input-bordered w-full max-w-xs"
+    />
+  );
+}
--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { Button } from "./common/button";
-
+import Input from "./common/Input";
 export default function Header() {
   return (
     <header className="w-full bg-accent h-16 flex justify-end items-center">
+      <Input textType={0} placeholder="please enter text" />
       <Button type={2} className="btn-secondary text-gray-950 mr-12">
         Log In
       </Button>
