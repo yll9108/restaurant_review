39e6043f2940bb4904a8dd0404bc68f3ddebd6ee
YenLun <yenlunliu@gmail.com>
2024-03-05 14:38:23 -0800

    fix tailwind issue

--- client/src/app/components/restaurant/restaurant.tsx
+++ client/src/app/components/restaurant/restaurant.tsx
@@ -3,19 +3,22 @@
 import React, { useEffect, useState } from "react";
 
 function Page() {
-  const [message, setMessage] = useState("Loading");
-  useEffect(() => {
-    fetch("http://localhost:8080/api/home")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMessage(data.message);
-      });
-  }, []);
+  // const [message, setMessage] = useState("Loading");
+  // useEffect(() => {
+  //   fetch("http://localhost:8080/api/home")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setMessage(data.message);
+  //     });
+  // }, []);
 
   return (
     <>
-      <div className="pb-5 text-4xl">{message}</div>
+      <div className="flex">
+        <div className="bg-primary">123</div>
+        <div>123</div>
+      </div>
     </>
   );
 }
