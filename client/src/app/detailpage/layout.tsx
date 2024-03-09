946dbf602cc3e32f34a8f1c48289d5252a6001aa
YenLun <yenlunliu@gmail.com>
2024-03-09 08:53:48 -0800

    add header

---
+++ client/src/app/detailpage/layout.tsx
@@ -0,0 +1,17 @@
+import "../globals.css";
+import Header from "@/components/header";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
