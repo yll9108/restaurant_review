51118106c21fdbcbc69f101b901c8e5e73cabad0
YenLun <yenlunliu@gmail.com>
2024-03-17 14:50:53 -0700

    toggle add reviews

--- client/src/app/components/review/NoReviews.tsx
+++ client/src/app/components/review/NoReviews.tsx
@@ -1,12 +1,28 @@
 import { Button } from "@/components/common/button";
-import React from "react";
+import React, { useState } from "react";
+import AddReview from "./AddReview";
 
 function NoReviews() {
+  // addReviews set as false, means you don't need to add Reviews now.
+  const [addReviews, setAddReviews] = useState(false);
+  const toggleAddReviews = () => {
+    setAddReviews(!addReviews);
+    console.log("setAddReviews", addReviews);
+  };
   return (
     <>
       <div className="flex flex-col items-center justify-center h-full">
         <div className="mb-5">No reviews</div>
-        <Button type={0}>Add reviews</Button>
+        <Button
+          type={0}
+          onClick={() => {
+            toggleAddReviews();
+          }}
+        >
+          Add reviews
+        </Button>
+        {/* when addReviews become true, means you need to addReviews now */}
+        {addReviews && <AddReview />}
       </div>
     </>
   );
