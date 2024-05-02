e098bed242129036e5488a93150e9e65e52a0019
masashi381 <121969638+masashi381@users.noreply.github.com>
2024-05-01 18:58:17 -0700

    creating users sign up

--- server/helpers/helpers.ts
+++ server/helpers/helpers.ts
@@ -1,14 +1,27 @@
 import { UserInput } from "../types/types";
+import userModels from "../models/userModels";
+
+export const createUser = (values: UserInput) => {
+  new userModels(values).save().then((user) => {
+    user.toObject();
+  });
+};
 
 export const validateUserInput = (
   userInput: UserInput
 ): { result: boolean; message: string } => {
   let result = false;
   let message = "";
+
   if (!userInput.user_email) {
     message = "Please enter your email address";
+  } else if (!userInput.user_name) {
+    message = "Please enter your name";
+  } else if (!userInput.user_password) {
+    message = "Please enter your password";
   } else {
     result = true;
   }
+
   return { result, message };
 };
