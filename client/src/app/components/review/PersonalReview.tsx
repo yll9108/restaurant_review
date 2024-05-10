e2f96cc5f7becdebc9cbda423a264c8fcb87cee7
YenLun <yenlunliu@gmail.com>
2024-05-09 22:26:25 -0700

    fetch review

--- client/src/app/components/review/PersonalReview.tsx
+++ client/src/app/components/review/PersonalReview.tsx
@@ -1,33 +1,45 @@
 import ReviewDetail from "@/components/common/ReviewDetail";
 import User from "@/components/common/User";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./custom.css";
 import { Button } from "@/components/common/button";
 import { dummyReviewList } from "./dummyReviewList";
 
 function PersonalReview() {
+  // fetch review
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch(
+      "http://localhost:8080/api/restaurants/6636eb854f21f73d829772c5/review/"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.warn("data", data);
+        setReviews(data);
+      });
+  }, []);
+
   const [showAllReviews, setShowAllReviews] = useState(false);
-  const displayAllReviews = showAllReviews
-    ? dummyReviewList
-    : dummyReviewList.slice(0, 5);
+  const displayAllReviews = showAllReviews ? reviews : reviews.slice(0, 5);
   const toggleReviews = () => {
     setShowAllReviews(!showAllReviews);
   };
+  console.log("displayAllReviews", displayAllReviews);
 
   return (
     <>
       <div className="flex flex-col">
         {displayAllReviews.map((review) => (
-          <div key={review.id} className="card bg-base-100 shadow-xl m-5">
+          <div key={review._id} className="card bg-base-100 shadow-xl m-5">
             <div className="card-body flexRow">
               <User />
               <ReviewDetail
-                id={review.id}
-                icon={review.icon}
-                rating={review.rating}
-                date={review.date}
-                title={review.title}
-                content={review.content}
+                id={review._id}
+                // icon={review.icon}
+                rating={review.review_ratings}
+                // date={review.date}
+                title={review.review_title}
+                content={review.review_description}
               />
             </div>
           </div>
